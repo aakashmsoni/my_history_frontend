f1591389a622c25b04b92daeabc1f09fa1e944b5
Aakash Soni <inos92@gmail.com>
2023-07-12 00:25:20 -0500

    created Index Events handle to get data via axios. Set useState for events

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,24 @@
+import axios from "axios";
+import { EventsIndex } from "./EventsIndex";
+import { useEffect, useState } from "react";
+// import { useState, useEffect } from "react";
+
 export function Content() {
+  const [events, setEvents] = useState([]);
+  // EVENTS INDEX
+  const handleIndexEvents = () => {
+    axios.get("http://localhost:3000/dates.json").then((response) => {
+      console.log(response);
+      setEvents(response.data);
+    });
+  };
+
+  useEffect(handleIndexEvents, []);
+
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <h1>Welcome to MyHistory!</h1>
+      <EventsIndex events={events} />
     </div>
   );
 }
