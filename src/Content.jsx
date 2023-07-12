31b13b173b89d548b85498d1d679695967d25f54
Aakash Soni <inos92@gmail.com>
2023-07-12 01:02:08 -0500

    installed sass, update vite.config with scoped scss plugin

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 import { EventsIndex } from "./EventsIndex";
 import { useEffect, useState } from "react";
-// import { useState, useEffect } from "react";
+
 
 export function Content() {
   const [events, setEvents] = useState([]);
   // EVENTS INDEX
   const handleIndexEvents = () => {
     axios.get("http://localhost:3000/dates.json").then((response) => {
-      console.log(response);
+      console.log(response.data);
       setEvents(response.data);
     });
   };
