d6751da488388156e64b36d9bf262896e538957e
Aakash Soni <inos92@gmail.com>
2023-07-12 01:41:23 -0500

    fixed Header to top, added img and app name

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,19 @@
 export function Header() {
   return (
     <header>
-      <nav>
-        <a href="#">Home</a> | <a href="#">Link</a>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img
+              src="src/assets/logo.jpeg"
+              alt="Logo"
+              width="100"
+              height="77"
+              className="d-inline-block align-text-right p-2"
+            />
+            MyHistory
+          </a>
+        </div>
       </nav>
     </header>
   );
