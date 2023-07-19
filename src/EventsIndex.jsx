dd45fc91e088d2d8328a633bc8fca06a84c8585e
Aakash Soni <inos92@gmail.com>
2023-07-18 20:28:51 -0500

    updated component to return selected date as header once defined

--- src/EventsIndex.jsx
+++ src/EventsIndex.jsx
@@ -1,9 +1,21 @@
 export function EventsIndex(props) {
   return (
     <div id="events-index">
-      <h1>Events Index</h1>
-      {props.events.map((event) => (
-        <div key={event.year}>{event.event}</div>
+      <h1>
+        {props.events[0] === undefined ? (
+          <div>
+            <h3>select a date</h3>
+          </div>
+        ) : (
+          <div>
+            {props.events[0].month}/{props.events[0].day}
+          </div>
+        )}
+      </h1>
+      {props.events.map(event => (
+        <div key={event.year}>
+          {event.year} - {event.event}
+        </div>
       ))}
     </div>
   );
