6f88ddf3183a7260fa41f33133414ea5f8baabd9
Aakash Soni <inos92@gmail.com>
2023-07-18 20:30:30 -0500

    removed useEffect

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,23 +1,53 @@
 import axios from "axios";
 import { EventsIndex } from "./EventsIndex";
 import { DateInput } from "./DateInput";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export function Content() {
   const [events, setEvents] = useState([]);
+
   // EVENTS INDEX
-  const handleIndexEvents = () => {
-    axios.get("http://localhost:3000/dates.json").then(response => {
+  const handleIndexEvents = params => {
+    console.log(params);
+    axios.post("http://localhost:3000/dates.json", params).then(response => {
       console.log(response.data);
       setEvents(response.data);
     });
   };
 
-  useEffect(handleIndexEvents, []);
+  // const handleIndexEvents = () => {
+  //   axios.get("http://localhost:3000/dates.json").then(response => {
+  //     console.log(response.data);
+  //     setEvents(response.data);
+  //   });
+  // };
+
+  //   axios
+  //     .get("https://historical-events-by-api-ninjas.p.rapidapi.com/v1/historicalevents", {
+  //       params: {
+  //         month: "3",
+  //         day: "30",
+  //       },
+  //       headers: {
+  //         "X-RapidAPI-Key": "50a024a574msh462873517c4ab94p1896fajsn1b1276bd85e1",
+  //         "X-RapidAPI-Host": "historical-events-by-api-ninjas.p.rapidapi.com",
+  //       },
+  //     })
+  //     .then(function(response) {
+  //       console.log(response.data);
+  //       setEvents(response.data);
+  //     })
+  //     .catch(function(error) {
+  //       console.error(error);
+  //     });
+  //   // setEvents(response.data);
+  // };
+
+  // useEffect(handleIndexEvents, []);
 
   return (
     <div>
-      <DateInput />
+      <DateInput onSubmitParams={handleIndexEvents} />
       <EventsIndex events={events} />
     </div>
   );
